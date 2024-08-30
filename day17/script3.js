e6b51d6552a727cbbe752dc14893393dd2b4cbf5
Kundan72 <kundankrdas22@gmail.com>
2024-08-31 02:00:50 +0530

    functions

---
+++ day17/script3.js
@@ -0,0 +1,45 @@
+// LOOPS
+
+console.log("I am a tutorial on Loops")
+
+let a = 1;
+// console.log(a)
+// console.log(a+1)
+// console.log(a+2)
+
+// for (let i = 0; i < 100; i++) { 
+//     console.log(a + i); 
+// }
+
+let obj = {
+    name: "Kundan",
+    role: "Programmer",
+    company: "Kundan AI"
+}
+ 
+// for (const key in obj) {  
+//         console.log(key)
+// }
+
+// for(const key in obj){
+//     const element = obj[key];
+//     console.log(key, element)
+// }
+
+// for of
+// for (const c of "Kundan") {
+//     console.log(c)
+// }
+
+// let i = 0;
+// while (i<600) {
+//     console.log(i)
+//     i++;
+// }
+
+// do while
+let i = 10;
+do {
+    console.log(i)
+    i++;
+} while (i<6);
