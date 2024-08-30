c2867a796393cb267d60dd017cecb6d20049b9a6
Kundan72 <kundankrdas22@gmail.com>
2024-08-31 00:25:22 +0530

    js variables

--- day17/script.js
+++ day17/script.js
@@ -1,8 +1,20 @@
 console.log("running JavaScript 2nd day")
 
 var a = 5
+a = a + 1
 var b = 10
 var c = "Kundan"
 
 console.log(a, b, c)
-typeof(a, b, c)
+
+console.log(typeof a, typeof b, typeof c)
+console.log(a+b+8)
+
+// invalid datatype
+// var 55a = "Rohan"  should not start with number
+// var Harry = "Rohan"  case sensitive
+// var harry = "Mohan"  are different both are different
+
+
+// const a1 = 6
+// a1 = a1 + 1   not possible
