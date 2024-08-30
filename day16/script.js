94c6d2ac8cf896c84b0f6ac8d3a6eff1949f7980
Kundan72 <kundankrdas22@gmail.com>
2024-08-30 21:09:00 +0530

    js done

--- day16/script.js
+++ day16/script.js
@@ -1,3 +1,15 @@
-alert("Hello I am JavaScript");
+// alert("Hello I am JavaScript");
 
 console.log("JavaScript is running....");
+
+var number = prompt("Enter your number: ");
+console.log("your number is: "+number);
+
+var isTrue = confirm("Are you sure you want to leave this page ");
+
+if(isTrue){
+    console.log("Computer starts countdown");
+}
+else{
+    console.log("Computer is not blasting");
+}
