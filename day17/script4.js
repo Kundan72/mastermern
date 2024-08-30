dabbd8792def25aa5deb85a4d6e140557b9e7cd8
Kundan72 <kundankrdas22@gmail.com>
2024-08-31 02:24:04 +0530

    functions

---
+++ day17/script4.js
@@ -0,0 +1,33 @@
+// FUNCTIONS
+
+function nice(name) {
+    console.log("Hey " + name + " you are nice!")
+    console.log("Hey " + name + " you are good!")
+    console.log("Hey " + name + " your tshirt is nice!")
+}
+
+function sum(a, b, c = 3) {
+    // console.log(a + b)
+    console.log(a, b, c)
+    return a + b + c
+}
+
+
+result1 = sum(3, 2)
+result2 = sum(7, 5)
+result3 = sum(3, 13, 1)
+
+console.log("The sum of these numbers is: ", result1)
+console.log("The sum of these numbers is: ", result2)
+console.log("The sum of these numbers is: ", result3)
+
+
+// saving funct1 in a varible named x 
+const func1 = (x)=>{
+    console.log("I am an arrow function", x)
+    console.log("Heyyyyy")
+}
+
+func1(34);
+func1(66);
+func1(84);
