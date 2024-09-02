e22b02f3d33b6cbb25a15bef4dd1059c06913944
Kundan72 <kundankrdas22@gmail.com>
2024-09-02 20:03:13 +0530

    dom

---
+++ day19/index2.js
@@ -0,0 +1,13 @@
+document.title = "documentobject"
+console.log(document.body)
+
+// go through page 33 of notes i.e walking the DOM
+
+
+document.body.firstElementChild 
+document.body.firstElementChild.childNodes 
+document.body.firstElementChild.children
+
+
+alert(typeof doument)
+alert(typeof window)
